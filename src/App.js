a2cbe063d644e92b0924e258235761dbdd87749a
matheusbrito <matheusbritodasilva@gmail.com>
2021-01-14 12:11:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [nextUrl, setNextUrl] = useState("");
+  const [previousUrl, setPreviousUrl] = useState("");
+  const [list, setList] = useState([]);
+const retrive = async ( next) => {
+  let response ;
+    if(next){ 
+    response = await axios(nextUrl ? nextUrl : "https://pokeapi.co/api/v2/pokemon");
+    }else{
+      response = await axios(previousUrl ? previousUrl : "https://pokeapi.co/api/v2/pokemon");
+    }
+     
+      window.console.log(response);
+      setNextUrl(response.data.next);
+      setPreviousUrl(response.data.previous);
+      setList(response.data.results)
+      
+    };
+
+    
+  useEffect(() => {
+    retrive(true);
+    
+  }, []);
+  
+
+  return <>
+  <ul>{list.map((item, i) => (<li key={i}>
+    <a href={item.url}>{item.name}</a>
+  </li>))}</ul>
+  
+  <button onClick={()=>retrive(true)}>proxima</button>;
+  <button onClick={()=>retrive(false)} hidden={!previousUrl}>anterior</button>;
+  </> 
 }
 
 export default App;
